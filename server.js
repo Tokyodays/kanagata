0b51f0c76b3fc894eacc0e6d0d0472fad19c979c
Tokyodays <kikuchi@gmail.com>
2017-01-22 05:29:41 +0900

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var logger = require('morgan');
+var compression = require('compression');
+var serveStatic = require('serve-static');
+var basicAuth   = require('basic-auth-connect');
 
 var user = process.env.USER;
 var pass = process.env.PASS;
@@ -8,13 +11,13 @@ var pass = process.env.PASS;
 app.set('port', process.env.PORT || 3000);
 
 if (user && pass) {
-  app.use(express.basicAuth(user, pass));
+  app.use(basicAuth(user, pass));
 }
 
 //app.use(express.logger('dev'));
-app.use(logger);
-app.use(express.compress());
-app.use(express.static(__dirname + '/dist'));
+app.use(logger('dev'));
+app.use(compression());
+app.use(serveStatic(__dirname + '/dist'));
 
 app.listen(app.get('port'), function() {
   console.log('Server listening on port %s', app.get('port'));
